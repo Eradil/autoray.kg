edc3c05b24b4448bbebab7ec87852487034df688
Bekbolsu <bekbolsunabdykadyrov214@gmail.com>
2022-07-16 15:59:16 +0200

    card

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Collapse } from "antd";
+import price from "../Images/price.png";
+import choice from "../Images/choice.png";
+import deliver from "../Images/deliver.png";
+import return1 from "../Images/return.png";
+
+import garant from "../Images/garant.png";
+
 import "./AboutUs.css";
 
 const { Panel } = Collapse;
@@ -41,7 +48,7 @@ const AboutUs = () => {
         </div>
         <div className="about_cols_2">
           <h1 className="about-3_1-title">Наши преимущества</h1>
-          <Collapse className="about_cols_2_collapse" accordion>
+          {/* <Collapse className="about_cols_2_collapse" accordion>
             <Panel key="1">
               <p>{"Доступные цены"}</p>
             </Panel>
@@ -57,7 +64,27 @@ const AboutUs = () => {
             <Panel key="5">
               <p>{"Простой возврат"}</p>
             </Panel>
-          </Collapse>
+          </Collapse> */}
+          <div className="about__panel">
+            <img className="about__panel-img" src={price} alt="" />
+            <p className="about__panel-text">Доступные цены</p>
+          </div>
+          <div className="about__panel">
+            <img className="about__panel-img" src={choice} alt="" />
+            <p className="about__panel-text">Широкий выбор</p>
+          </div>
+          <div className="about__panel">
+            <img className="about__panel-img" src={deliver} alt="" />
+            <p className="about__panel-text">Быстрая доставка</p>
+          </div>
+          <div className="about__panel">
+            <img className="about__panel-img" src={garant} alt="" />
+            <p className="about__panel-text">Гарантия на товары</p>
+          </div>
+          <div className="about__panel">
+            <img className="about__panel-img" src={return1} alt="" />
+            <p className="about__panel-text">Простой возврат</p>
+          </div>
         </div>
       </div>
     </div>
